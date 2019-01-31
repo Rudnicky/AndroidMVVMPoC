b954a86f48639d4d28b8c11b51299a82c108ea1a
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-01-31 21:35:50 +0000

    Added TextView binded value, Added EditText onTextChanged.

--- app/src/main/java/com/example/androidmvvmpoc/viewmodels/MainActivityViewModel.java
+++ app/src/main/java/com/example/androidmvvmpoc/viewmodels/MainActivityViewModel.java
@@ -2,6 +2,9 @@ package com.example.androidmvvmpoc.viewmodels;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.text.Editable;
+import android.text.TextWatcher;
+
 import com.android.databinding.library.baseAdapters.BR;
 
 public class MainActivityViewModel extends BaseObservable {
@@ -9,12 +12,28 @@ public class MainActivityViewModel extends BaseObservable {
     @Bindable
     public String toastMessage = null;
 
+    @Bindable
+    public String text = null;
+
     public void onButtonClicked() {
         setToastMessage("Button clicked!");
     }
 
     public void onAfterTextChanged(CharSequence sequence) {
-        setToastMessage(sequence.toString());
+//        setToastMessage(sequence.toString());
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        setTextViewText(s.toString());
+    }
+
+    public String getTextViewText() {
+        return "Some random text";
+    }
+
+    public void setTextViewText(String text) {
+        this.text = text;
+        notifyPropertyChanged(BR.text);
     }
 
     public String getToastMessage() {
