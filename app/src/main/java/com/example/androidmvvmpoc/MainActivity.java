02325ee3ccd7f31992a28fdac99a2d11ad27613c
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-01-31 16:28:12 +0000

    Added data binding library to gradle

--- app/src/main/java/com/example/androidmvvmpoc/MainActivity.java
+++ app/src/main/java/com/example/androidmvvmpoc/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidmvvmpoc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.androidmvvmpoc.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
